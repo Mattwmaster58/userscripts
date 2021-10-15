3e60ac4690f2989ee75dcabba0f04c9631c2aece
Mattwmaster58 <mattwmaster58@gmail.com>
2021-10-15 17:48:21 -0600

    tab cleanup

--- template.js
+++ template.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name          	SCRIPT
+// @name            SCRIPT
 // @description     SCRIPT DESCRIPTION
-// @author			Mattwmaster58 <mattwmaster58@gmail.com>
+// @author          Mattwmaster58 <mattwmaster58@gmail.com>
 // @namespace       Mattwmaster58 Scripts
-// @match			http://*.example.com/*
+// @match           http://*.example.com/*
 // @version         0.1
 // ==/UserScript==
 
