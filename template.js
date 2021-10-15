69a42080614c35ee3bb7f8ef17cc6e407a8b2e5a
Mattwmaster58 <mattwmaster58@gmail.com>
2021-10-15 10:48:31 -0600

    remove whitespace

--- template.js
+++ template.js
@@ -1,15 +1,10 @@
 // ==UserScript==
 // @name          	SCRIPT
 // @description     SCRIPT DESCRIPTION
-//
 // @author			Mattwmaster58 <mattwmaster58@gmail.com>
 // @namespace       Mattwmaster58 Scripts
-// @downloadURL		https://raw.github.com/Mattwmaster58/userscripts/master/<SCRIPT>.user.js
-//
 // @match			http://*.example.com/*
-//
 // @version         0.1
-// @updateURL		https://raw.github.com/Mattwmaster58/userscripts/master/<SCRIPT>.user.js
 // ==/UserScript==
 
 (function() {
