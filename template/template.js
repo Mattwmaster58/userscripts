3837c467d7471897d14afc1f70b6c5ed2e677d00
Mattwmaster58 <mattwmaster58@gmail.com>
2022-06-06 09:32:17 -0400

    remove redundant spread operation

--- template/template.js
+++ template/template.js
@@ -8,13 +8,13 @@
 // ==/UserScript==
 
 function _log(...args) {
-  return console.log(...["%c[]", "color: green", ...args]);
+  return console.log("%c[]", "color: green", ...args);
 }
 
 function _warn(...args) {
-  return console.log(...["%c[]", "color: yellow", ...args]);
+  return console.log("%c[]", "color: yellow", ...args);
 }
 
 function _debug(...args) {
-  return console.log(...["%c[]", "color: green", ...args]);
+  return console.log("%c[]", "color: green", ...args);
 }
